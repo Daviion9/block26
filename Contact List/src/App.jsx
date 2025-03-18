9d5478d14bbae9e2bae7f601b11af28f606a6dae
Daviion9 <daviionburks9@gmail.com>
2025-03-18 18:05:20 -0400

    commit

--- Contact List/src/App.jsx
+++ Contact List/src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from "react";
+import './App.css';
+import ContactList from "./components/ContactList";
+import SelectedContact from "./components/SelectedContact";
 
-import './App.css'
 
 
 export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
+
+
+  export default function ContactRow({ setSelectedContactId, contact }) {
+    return (
+      <tr
+        onClick={() => {
+          setSelectedContactId(contact.id);
+        }}
+      >
+        <td>{contact.name}</td>
+        <td>{contact.email}</td>
+        <td>{contact.phone}</td>
+      </tr>
+    );
+  }
   
   return (
     <>
-      <ContactList />
+      {selectedContactId ? (
+        <div>Selected Contact View</div>
+      ) : (
+        <ContactList />
+      )}
     </>
   );
 }
-
-// export default App
